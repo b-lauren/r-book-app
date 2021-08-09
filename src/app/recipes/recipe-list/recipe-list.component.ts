244d27370ac5d6d346442a0640ae33d6fe826d91
Rebecca Ross <rebecca.ross@smartbnb.io>
2021-08-09 21:20:59 +0100

    Extra functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import * as EventEmitter from 'events';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +8,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
       'This is a test',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2020_50/1650098/google-recipe-trends-main-khu-201209.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is the second test',
       'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2020_50/1650098/google-recipe-trends-main-khu-201209.jpg'
     ),
   ];
@@ -18,4 +25,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
